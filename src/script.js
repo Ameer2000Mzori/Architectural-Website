fed2d739b4a4b841208af6d1cabc09c13c0dd838
Ameer <ameerapex496@gmail.com>
2023-12-23 15:25:07 +0100

    added comments + selected elements + added forEach func

--- src/script.js
+++ src/script.js
@@ -1,1 +1,9 @@
-console.log("hallo world");
+// selecting elements
+var aElements = document.querySelectorAll("a");
+// gelobal varibales
+var elementsCount = 0;
+// functions
+// event linsters
+aElements.forEach(function (aElement) {
+    console.log(aElement);
+});
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,12 @@
-console.log("hallo world");
+// selecting elements
+const aElements = document.querySelectorAll("a");
+
+// gelobal varibales
+let elementsCount = 0;
+
+// functions
+
+// event linsters
+aElements.forEach((aElement) => {
+  console.log(aElement);
+});
