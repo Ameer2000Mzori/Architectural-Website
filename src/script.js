83250b1cbfc0bcea262916a87dcdddaf4f8c5e31
Ameer <ameerapex496@gmail.com>
2023-12-23 15:42:14 +0100

    added timeout logic

--- src/script.js
+++ src/script.js
@@ -1,9 +1,35 @@
 // selecting elements
 var aElements = document.querySelectorAll("a");
+var burgerMenuBtn = document.getElementsByClassName("burger-Menu-Wrap")[0];
 // gelobal varibales
 var elementsCount = 0;
 // functions
 // event linsters
-aElements.forEach(function (aElement) {
-    console.log(aElement);
-});
+var runEffect = function () {
+    var giveClassInt = setInterval(function () {
+        if (elementsCount < aElements.length) {
+            aElements[elementsCount].classList.add("acitve");
+        }
+        else {
+            console.log("finished");
+            setTimeout(function () {
+                clearInterval(giveClassInt);
+            }, 0);
+        }
+    }, 1000);
+    var increaseNum = setInterval(function () {
+        if (elementsCount <= aElements.length) {
+            elementsCount++;
+            console.log(elementsCount);
+        }
+        else {
+            setTimeout(function () {
+                clearInterval(increaseNum);
+            }, 0);
+        }
+    }, 1500);
+    console.log(elementsCount);
+};
+// aElements.forEach((aElement) => {});
+// const giveClassInt = setInterval(runEffect, 10000);
+burgerMenuBtn.addEventListener("click", runEffect);
--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,6 @@
 // selecting elements
 const aElements = document.querySelectorAll("a");
+const burgerMenuBtn = document.getElementsByClassName("burger-Menu-Wrap")[0];
 
 // gelobal varibales
 let elementsCount = 0;
@@ -7,6 +8,31 @@ let elementsCount = 0;
 // functions
 
 // event linsters
-aElements.forEach((aElement) => {
-  console.log(aElement);
-});
+const runEffect = () => {
+  const giveClassInt = setInterval(() => {
+    if (elementsCount < aElements.length) {
+      aElements[elementsCount].classList.add("acitve");
+    } else {
+      console.log("finished");
+      setTimeout(() => {
+        clearInterval(giveClassInt);
+      }, 0);
+    }
+  }, 1000);
+
+  const increaseNum = setInterval(() => {
+    if (elementsCount <= aElements.length) {
+      elementsCount++;
+      console.log(elementsCount);
+    } else {
+      setTimeout(() => {
+        clearInterval(increaseNum);
+      }, 0);
+    }
+  }, 1500);
+  console.log(elementsCount);
+};
+
+// aElements.forEach((aElement) => {});
+// const giveClassInt = setInterval(runEffect, 10000);
+burgerMenuBtn.addEventListener("click", runEffect);
